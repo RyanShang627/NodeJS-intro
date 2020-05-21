718bdccd06d5e51a253226f3c55a5d49d0dfb2fb
RyanShang627 <ryan.d.shang@gmail.com>
2020-05-20 20:08:14 -0700

    feat: Read input from user

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,17 @@
 const chalk = require('chalk');
 
-console.log(chalk.green('Success!'));
+// console.log(chalk.green('Success!'));
 
-console.log(chalk.bold.red.inverse('Error'));
+// console.log(chalk.bold.red.inverse('Error'));
+
+// console.log(process.argv[2]);
+
+const command = process.argv[2];
+
+// console.log(process.argv);
+
+if (command === 'add') {
+  console.log('Adding note!');
+} else if (command === 'remove') {
+  console.log('Removing note!');
+}
